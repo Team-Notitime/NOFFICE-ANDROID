77692a4b19cd02b5e6d26d7e02f49b362f9c009f
easyhz <jiton3@naver.com>
2024-07-04 12:22:24 +0900

    chore: :data:my 모듈 추가

---
+++ data/my/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+}
+
+android {
+    namespace = "com.easyhz.noffice.data.my"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+}
