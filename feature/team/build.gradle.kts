cd5b73d4454dec8dfdef804b57c92a1e2b8c174f
easyhz <jiton3@naver.com>
2024-07-04 13:01:49 +0900

    chore: :feature:team 설정

---
+++ feature/team/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.noffice.android.library.compose)
+    alias(libs.plugins.noffice.android.feature)
+    alias(libs.plugins.noffice.android.hilt)
+}
+
+android {
+    namespace = "com.easyhz.noffice.feature.team"
+}
+
+dependencies {
+
+}
