6febcf9ee7a30b46116d8266ebdad22a65327f1d
easyhz <jiton3@naver.com>
2024-07-04 13:43:21 +0900

    chore: :core:network 설정

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,36 @@
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+}
+
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
+android {
+    namespace = "com.easyhz.noffice.core.network"
+    buildTypes {
+        debug {
+            buildConfigField("String", "NOFFICE_BASE_URL", localProperties["noffice.base.url.dev"].toString())
+        }
+        release {
+            buildConfigField("String", "NOFFICE_BASE_URL", localProperties["noffice.base.url.prod"].toString())
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
