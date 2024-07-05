1883b2e1f964608bea1a7202c874dcd6464e77cf
easyhz <jiton3@naver.com>
2024-07-05 18:43:58 +0900

    design: scaffold 및 theme 기본 작업

---
+++ core/design-system/src/main/java/com/easyhz/noffice/core/design_system/component/scaffold/NofficeScaffold.kt
@@ -0,0 +1,38 @@
+package com.easyhz.noffice.core.design_system.component.scaffold
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun NofficeScaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = { },
+    bottomBar: @Composable () -> Unit = { },
+    snackBarHost: @Composable () -> Unit = { },
+    floatingActionButton: @Composable () -> Unit = { },
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    containerColor: Color = Color.Transparent,
+    contentColor: Color = Color.Black, // FIXME
+    contentWindowInsets: WindowInsets = ScaffoldDefaults.contentWindowInsets,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackBarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        contentColor = contentColor,
+        containerColor =  containerColor,
+        contentWindowInsets = contentWindowInsets,
+    ) {
+        content(it)
+    }
+}
---
+++ core/design-system/src/main/java/com/easyhz/noffice/core/design_system/theme/theme.kt
@@ -0,0 +1,54 @@
+package com.easyhz.noffice.core.design_system.theme
+
+import android.app.Activity
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun NofficeTheme(
+    content: @Composable () -> Unit
+) {
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+    CompositionLocalProvider(
+        LocalRippleTheme provides NoRippleTheme,
+        LocalOverscrollConfiguration provides null,
+    ) {
+        MaterialTheme(
+            content = content
+        )
+    }
+}
+
+// no ripple
+object NoRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor(): Color = Color.Unspecified
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleAlpha(
+        draggedAlpha = 0f,
+        focusedAlpha = 0f,
+        hoveredAlpha = 0f,
+        pressedAlpha = 0f,
+    )
+}
