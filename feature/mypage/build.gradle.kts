9cf2336e781fcbb34472ffbc10256dcee60e85c7
easyhz <jiton3@naver.com>
2024-07-07 17:10:19 +0900

    chore: feature:mypage 모듈 추가

---
+++ feature/mypage/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.noffice.android.library.compose)
+    alias(libs.plugins.noffice.android.feature)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+}
+
+
+android {
+    namespace = "com.easyhz.noffice.feature.mypage"
+}
+
+dependencies {
+
+}
