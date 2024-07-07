1520c8b01a652224456655fd443c7794789ac2d3
easyhz <jiton3@naver.com>
2024-07-07 17:14:44 +0900

    chore: data:announcement 모듈 추가

---
+++ data/announcement/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+    alias(libs.plugins.noffice.android.application.firebase)
+}
+
+
+android {
+    namespace = "com.easyhz.noffice.data.announcement"
+}
+
+dependencies {
+
+}
