4e70d02b0a52536b5697aca80b1ac6df14cf7708
easyhz <jiton3@naver.com>
2024-07-08 14:21:30 +0900

    Design: color 추가

---
+++ core/design-system/src/main/java/com/easyhz/noffice/core/design_system/theme/Color.kt
@@ -0,0 +1,78 @@
+package com.easyhz.noffice.core.design_system.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+
+@Stable
+val White = Color(0xFFFFFFFF)
+@Stable
+val Grey50 = Color(0xFFFAFAFA)
+@Stable
+val Grey100 = Color(0xFFF4F4F4)
+@Stable
+val Grey200 = Color(0xFFE8E8E8)
+@Stable
+val Grey300 = Color(0xFFD6D6D6)
+@Stable
+val Grey400 = Color(0xFFB9B9B9)
+@Stable
+val Grey500 = Color(0xFF969696)
+@Stable
+val Grey600 = Color(0xFF787878)
+@Stable
+val Grey700 = Color(0xFF5B5B5B)
+@Stable
+val Grey800 = Color(0xFF3F3F3F)
+@Stable
+val Grey850 = Color(0xFF2C2C2C)
+@Stable
+val Grey900 = Color(0xFF212121)
+
+
+@Stable
+val Green100 = Color(0xFFEEFCF2)
+@Stable
+val Green200 = Color(0xFFC3F6D2)
+@Stable
+val Green300 = Color(0xFF97EDB3)
+@Stable
+val Green400 = Color(0xFF6CE594)
+@Stable
+val Green500 = Color(0xFF40DD74)
+@Stable
+val Green600 = Color(0xFF23C75A)
+@Stable
+val Green700 = Color(0xFF1C9C46)
+@Stable
+val Green800 = Color(0xFF147133)
+@Stable
+val Green900 = Color(0xFF0C451F)
+
+@Stable
+val Yellow100 = Color(0xFFFFFBEB)
+@Stable
+val Yellow200 = Color(0xFFFFF2B8)
+@Stable
+val Yellow300 = Color(0xFFFFE985)
+@Stable
+val Yellow400 = Color(0xFFFFDF52)
+@Stable
+val Yellow500 = Color(0xFFFFD61F)
+@Stable
+val Yellow600 = Color(0xFFEBC000)
+@Stable
+val Yellow700 = Color(0xFFB89600)
+@Stable
+val Yellow800 = Color(0xFF856C00)
+@Stable
+val Yellow900 = Color(0xFF524300)
+
+@Stable
+val Blue100 = Color(0xFFEBF9FF)
+@Stable
+val Blue200 = Color(0xFFB8E8FF)
+@Stable
+val Blue300 = Color(0xFF85D8FF)
+@Stable
+val Blue400 = Color(0xFF52C8FF)
