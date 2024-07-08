edfeb4309b5558a869f171b5ead4641e508f6ddb
easyhz <jiton3@naver.com>
2024-07-08 09:44:56 +0900

    chore: data:auth 모듈 추가

---
+++ data/auth/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+}
+
+
+android {
+    namespace = "com.easyhz.noffice.data.auth"
+}
+
+dependencies {
+
+}
