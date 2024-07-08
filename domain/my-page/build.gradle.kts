ff95a15c5b420adac00faa987c9d5254cf230bab
easyhz <jiton3@naver.com>
2024-07-08 11:09:41 +0900

    chore: domain:my-page 모듈 추가

---
+++ domain/my-page/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+}
+
+android {
+    namespace = "com.easyhz.noffice.domain.my_page"
+}
+
+dependencies {
+
+}
