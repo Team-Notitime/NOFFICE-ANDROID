bd8702fd6d2ea79f18474acbdae025f04c592974
easyhz <jiton3@naver.com>
2024-07-08 22:13:49 +0900

    fix: 기본 화면 변경

--- feature/home/src/main/java/com/easyhz/noffice/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/easyhz/noffice/feature/home/HomeScreen.kt
@@ -1,14 +1,13 @@
 package com.easyhz.noffice.feature.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.easyhz.noffice.core.design_system.component.scaffold.NofficeScaffold
 
 @Composable
@@ -20,14 +19,8 @@ fun HomeScreen() {
             }
         }
     ) {
-        LazyColumn(
-            modifier = Modifier.padding(it)
-        ) {
-            items(50) {
-                Text(text = "$it .. HomeScreen", modifier = Modifier
-                    .fillMaxWidth()
-                    .height(32.dp))
-            }
+        Box(modifier = Modifier.fillMaxSize().padding(it)) {
+            Text(text = "home", modifier = Modifier.align(Alignment.Center))
         }
     }
 }
