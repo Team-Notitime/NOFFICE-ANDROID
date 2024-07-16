2bdc98d63cb8d97b8556e377c1f854b709aa8cf3
easyhz <jiton3@naver.com>
2024-07-16 17:49:33 +0900

    fix: 어노테이션 추가

--- app/src/main/java/com/easyhz/noffice/MainActivity.kt
+++ app/src/main/java/com/easyhz/noffice/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.easyhz.noffice.core.design_system.theme.NofficeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
