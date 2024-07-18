9652aa062abec82f1ec7369dd3b11c24327c699b
easyhz <jiton3@naver.com>
2024-07-18 16:10:12 +0900

    feat: organization ui #19

---
+++ feature/organization/src/main/java/com/easyhz/noffice/feature/organization/contract/organization/OrganizationSideEffect.kt
@@ -0,0 +1,6 @@
+package com.easyhz.noffice.feature.organization.contract.organization
+
+import com.easyhz.noffice.core.common.base.UiSideEffect
+
+sealed class OrganizationSideEffect: UiSideEffect() {
+}
