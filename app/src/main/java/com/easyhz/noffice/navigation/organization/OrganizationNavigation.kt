e7e13677c198bf96401e549c77d941b43979132f
easyhz <jiton3@naver.com>
2024-07-18 09:14:36 +0900

    design: organization 메뉴 이름 변경 및 패딩 설정

--- app/src/main/java/com/easyhz/noffice/navigation/organization/OrganizationNavigation.kt
+++ app/src/main/java/com/easyhz/noffice/navigation/organization/OrganizationNavigation.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.easyhz.noffice.feature.organization.OrganizationScreen
+import com.easyhz.noffice.feature.organization.screen.organization.OrganizationScreen
 import com.easyhz.noffice.navigation.organization.screen.Organization
 
 internal fun NavGraphBuilder.organizationScreen(modifier: Modifier) {
