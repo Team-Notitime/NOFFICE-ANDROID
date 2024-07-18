8540830636ffa3d81ad2147afbca57abd48fdf1e
easyhz <jiton3@naver.com>
2024-07-18 15:44:44 +0900

    design: box 제거

--- core/design-system/src/main/java/com/easyhz/noffice/core/design_system/component/exception/ExceptionView.kt
+++ core/design-system/src/main/java/com/easyhz/noffice/core/design_system/component/exception/ExceptionView.kt
@@ -2,10 +2,10 @@ package com.easyhz.noffice.core.design_system.component.exception
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,42 +20,41 @@ import com.easyhz.noffice.core.design_system.theme.CardExceptionTitle
 import com.easyhz.noffice.core.design_system.util.exception.ExceptionType
 
 @Composable
-internal fun ExceptionView(
+fun ExceptionView(
     modifier: Modifier = Modifier,
     type: ExceptionType
 ) {
-    Box(modifier = modifier) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp)
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = type.resId),
+            contentDescription = "no_group"
+        )
+        Column(modifier = Modifier
+            .padding(top = 10.dp)
+            .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = type.resId),
-                contentDescription = "no_group"
+            Text(
+                modifier = Modifier,
+                text = stringResource(id = type.titleStringId),
+                textAlign = TextAlign.Center,
+                style = CardExceptionTitle,
             )
-            Column(modifier = Modifier
-                .fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(6.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    modifier = Modifier,
-                    text = stringResource(id = type.titleStringId),
-                    textAlign = TextAlign.Center,
-                    style = CardExceptionTitle,
-                )
-                Text(
-                    modifier = Modifier,
-                    text = stringResource(id = type.subTitleStringId),
-                    textAlign = TextAlign.Center,
-                    style = CardExceptionSubTitle,
-                )
-            }
+            Text(
+                modifier = Modifier,
+                text = stringResource(id = type.subTitleStringId),
+                textAlign = TextAlign.Center,
+                style = CardExceptionSubTitle,
+            )
         }
     }
+
 }
 
 @Preview
