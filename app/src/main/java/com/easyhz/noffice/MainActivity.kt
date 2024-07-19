289db5c855666fc9ac6a0817ed7abe919cf3616d
easyhz <jiton3@naver.com>
2024-07-19 13:19:30 +0900

    fix: unused import directive

--- app/src/main/java/com/easyhz/noffice/MainActivity.kt
+++ app/src/main/java/com/easyhz/noffice/MainActivity.kt
@@ -3,7 +3,6 @@ package com.easyhz.noffice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
 import com.easyhz.noffice.core.design_system.theme.NofficeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
