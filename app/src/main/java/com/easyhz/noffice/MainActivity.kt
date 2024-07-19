398932740ab55769a3821d0396522108ccb3c1f8
easyhz <jiton3@naver.com>
2024-07-19 13:00:35 +0900

    fix: 윈도우 에셋 수정

--- app/src/main/java/com/easyhz/noffice/MainActivity.kt
+++ app/src/main/java/com/easyhz/noffice/MainActivity.kt
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             NofficeTheme {
                 NofficeApp()
