4b6bd4d6232097dd1c14370d5033b9a460730ad1
easyhz <jiton3@naver.com>
2024-07-22 16:38:22 +0900

    feat: login ui #31

--- app/src/main/java/com/easyhz/noffice/navigation/sign/SignNavigation.kt
+++ app/src/main/java/com/easyhz/noffice/navigation/sign/SignNavigation.kt
@@ -2,10 +2,15 @@ package com.easyhz.noffice.navigation.sign
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.easyhz.noffice.feature.sign.screen.login.LoginScreen
 import com.easyhz.noffice.feature.sign.screen.signUp.SignUpScreen
+import com.easyhz.noffice.navigation.sign.screen.LogIn
 import com.easyhz.noffice.navigation.sign.screen.SignUp
 
 internal fun NavGraphBuilder.signScreen() {
+    composable<LogIn> {
+        LoginScreen()
+    }
     composable<SignUp> {
         SignUpScreen()
     }
--- core/design-system/src/main/java/com/easyhz/noffice/core/design_system/extension/Modifier.kt
+++ core/design-system/src/main/java/com/easyhz/noffice/core/design_system/extension/Modifier.kt
@@ -1,5 +1,6 @@
 package com.easyhz.noffice.core.design_system.extension
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -37,3 +41,55 @@ fun Modifier.borderBottom(color: Color, width: Dp): Modifier = this.drawBehind {
         strokeWidth = strokeWidth
     )
 }
+
+
+/**
+ * 그림자 효과 [Modifier] 확장 함수 - 버튼 기준
+ *
+ * @param shadowColor 그림자 색
+ * @param borderRadius 버튼 radius
+ * @param blurRadius 블러 radius
+ * @param offsetY y 오프셋
+ * @param offsetX x 오프셋
+ * @param spread 얼마나 퍼질건지
+ * @param modifier Modifier
+ *
+ * @return [Modifier]
+ */
+fun Modifier.buttonShadowEffect(
+    shadowColor: Color = Color.Black,
+    borderRadius: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0f.dp,
+    modifier: Modifier = Modifier
+) = this.then(
+    modifier.drawBehind {
+        this.drawIntoCanvas {
+            val paint = Paint()
+            val frameworkPaint = paint.asFrameworkPaint()
+            val spreadPixel = spread.toPx()
+            val leftPixel = (0f - spreadPixel) + offsetX.toPx()
+            val topPixel = (0f - spreadPixel) + offsetY.toPx()
+            val rightPixel = (this.size.width + spreadPixel)
+            val bottomPixel = (this.size.height + spreadPixel)
+
+            if (blurRadius != 0.dp) {
+                frameworkPaint.maskFilter =
+                    (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
+            }
+
+            frameworkPaint.color = shadowColor.toArgb()
+            it.drawRoundRect(
+                left = leftPixel,
+                top = topPixel,
+                right = rightPixel,
+                bottom = bottomPixel,
+                radiusX = borderRadius.toPx(),
+                radiusY = borderRadius.toPx(),
+                paint
+            )
+        }
+    }
+)
