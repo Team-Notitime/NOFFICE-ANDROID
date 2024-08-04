37ecd962abe534a295196eab2815562628e013dd
easyhz <jiton3@naver.com>
2024-08-04 22:17:16 +0900

    feat: announcement response 추가 #38

---
+++ core/network/src/main/java/com/easyhz/noffice/core/network/model/response/announcement/AnnouncementResponse.kt
@@ -0,0 +1,19 @@
+package com.easyhz.noffice.core.network.model.response.announcement
+
+data class AnnouncementResponse(
+    val announcements: List<AnnouncementItem>
+)
+data class AnnouncementItem(
+    val announcementId: Int,
+    val content: String,
+    val createdAt: String,
+    val endAt: String,
+    val memberId: Int,
+    val noticeAt: List<String>,
+    val organizationId: Int,
+    val placeLinkName: String,
+    val placeLinkUrl: String,
+    val profileImageUrl: String,
+    val title: String,
+    val updatedAt: String
+)
