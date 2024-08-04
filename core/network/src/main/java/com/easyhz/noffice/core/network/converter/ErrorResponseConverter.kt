3fec9dfa1fa6f697a1e41fa1d2b0395266fedc7f
easyhz <jiton3@naver.com>
2024-08-04 19:56:33 +0900

    fix: network 모듈 수정 #38

--- core/network/src/main/java/com/easyhz/noffice/core/network/converter/ErrorResponseConverter.kt
+++ core/network/src/main/java/com/easyhz/noffice/core/network/converter/ErrorResponseConverter.kt
@@ -2,8 +2,6 @@ package com.easyhz.noffice.core.network.converter
 
 import com.easyhz.noffice.core.network.model.response.ErrorResponse
 import com.google.gson.Gson
-import dagger.Provides
-import javax.inject.Inject
 
 class ErrorResponseConverter constructor(
     private val gson: Gson
