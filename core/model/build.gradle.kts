d543260bacef6218852626af3c5d67f435622b06
easyhz <jiton3@naver.com>
2024-08-06 20:17:00 +0900

    chore: 설정 변경

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.library.compose)
     alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.easyhz.noffice.core.model"
 }
 
 dependencies {
