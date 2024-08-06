5f5ff8a1a4e9598b7fd63dc3288444c24474fbaa
easyhz <jiton3@naver.com>
2024-08-06 14:32:49 +0900

    fix: 어노테이션 추가

--- core/model/src/main/java/com/easyhz/noffice/core/model/organization/OrganizationInformation.kt
+++ core/model/src/main/java/com/easyhz/noffice/core/model/organization/OrganizationInformation.kt
@@ -1,7 +1,10 @@
 package com.easyhz.noffice.core.model.organization
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class OrganizationInformation(
-//    val id: Int,
+    val id: Int,
     val name: String,
     val profileImageUrl: String,
     val category: List<String>,
