d34dc38147bcede46b642b16f9db8eeda3a594a7
easyhz <jiton3@naver.com>
2024-08-07 17:19:11 +0900

    feat: Member type string 추가

--- core/model/src/main/java/com/easyhz/noffice/core/model/organization/member/MemberType.kt
+++ core/model/src/main/java/com/easyhz/noffice/core/model/organization/member/MemberType.kt
@@ -4,11 +4,14 @@ import androidx.annotation.StringRes
 import com.easyhz.noffice.core.model.R
 
 enum class MemberType(
-    @StringRes val stringId: Int
+    @StringRes val stringId: Int,
+    @StringRes val subStringId: Int,
 ) {
     LEADER(
-        stringId = R.string.organization_member_type_leader
+        stringId = R.string.organization_member_type_leader,
+        subStringId = R.string.organization_member_type_leader_sub
     ), MEMBER(
-        stringId = R.string.organization_member_type_member
+        stringId = R.string.organization_member_type_member,
+        subStringId = R.string.organization_member_type_member_sub,
     )
 }
