3409d6f4eb2ea6c604149c81c5230feb0cf246e4
easyhz <jiton3@naver.com>
2024-08-09 14:07:12 +0900

    feat: my page 추가 #43

---
+++ feature/my-page/src/main/java/com/easyhz/noffice/feature/my_page/contract/MyPageState.kt
@@ -0,0 +1,23 @@
+package com.easyhz.noffice.feature.my_page.contract
+
+import com.easyhz.noffice.core.common.base.UiState
+import com.easyhz.noffice.core.model.profile.User
+
+data class MyPageState(
+    val user: User,
+    val isCheckedNotification: Boolean,
+    val isShowImageBottomSheet: Boolean
+): UiState() {
+    companion object {
+        fun init() = MyPageState(
+            user = User(
+                id = "",
+                email = "guest@noffice.com",
+                name = "푸바옹",
+                profileImageUrl = ""
+            ),
+            isCheckedNotification = true,
+            isShowImageBottomSheet = false
+        )
+    }
+}
---
+++ feature/my-page/src/main/java/com/easyhz/noffice/feature/my_page/util/MyPageSection.kt
@@ -0,0 +1,88 @@
+package com.easyhz.noffice.feature.my_page.util
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
+import com.easyhz.noffice.core.design_system.R
+import com.easyhz.noffice.core.design_system.theme.Grey800
+import com.easyhz.noffice.core.design_system.theme.Red
+
+
+enum class MyPageSection(
+    @StringRes
+    override val titleId: Int,
+    val items: List<MyPageMenu>
+) : Menu {
+    GUIDE(
+        titleId = R.string.my_page_menu_title_guide,
+        items = listOf(
+            MyPageMenu.VERSION,
+            MyPageMenu.INQUIRY,
+            MyPageMenu.NOTICE,
+            MyPageMenu.TERMS_OF_SERVICE,
+            MyPageMenu.PRIVACY_POLICY
+        )
+    ),
+    SETTING(
+        titleId = R.string.my_page_menu_title_setting,
+        items = listOf(MyPageMenu.NOTIFICATION)
+    ),
+    ETC(
+        titleId = R.string.my_page_menu_title_etc,
+        items = listOf(
+            MyPageMenu.CONSENT_TO_INFORMATION,
+            MyPageMenu.DELETE_ACCOUNT,
+            MyPageMenu.SIGN_OUT
+        )
+    )
+}
+
+enum class MyPageMenu(
+    @StringRes
+    override val titleId: Int,
+    val type: MenuType = MenuType.MORE,
+    val textColor: Color = Grey800
+) : Menu {
+    VERSION(
+        titleId = R.string.my_page_menu_version,
+        type = MenuType.VERSION_TEXT
+    ),
+    INQUIRY(
+        titleId = R.string.my_page_menu_inquiry,
+    ),
+    NOTICE(
+        titleId = R.string.my_page_menu_notice,
+    ),
+    TERMS_OF_SERVICE(
+        titleId = R.string.my_page_menu_terms_of_service
+    ),
+    PRIVACY_POLICY(
+        titleId = R.string.my_page_menu_privacy_policy
+    ),
+    NOTIFICATION(
+        titleId = R.string.my_page_menu_notification,
+        type = MenuType.TOGGLE
+    ),
+    CONSENT_TO_INFORMATION(
+        titleId = R.string.my_page_menu_consent_to_information
+    ),
+    DELETE_ACCOUNT(
+        titleId = R.string.my_page_menu_delete_account,
+        textColor = Red
+    ),
+    SIGN_OUT(
+        titleId = R.string.my_page_menu_sign_out,
+        textColor = Red
+    )
+}
+
+enum class MenuType {
+    MORE, TOGGLE, VERSION_TEXT;
+
+    fun isClickable() : Boolean = this != VERSION_TEXT
+}
+
+internal interface Menu {
+    val titleId: Int
+}
+
+
