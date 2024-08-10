132ab4b5078ab50be5417e1606969535eba81042
easyhz <jiton3@naver.com>
2024-08-10 10:50:01 +0900

    chore: 모듈 의존성 추가 #52

--- feature/sign/build.gradle.kts
+++ feature/sign/build.gradle.kts
@@ -12,6 +12,7 @@ android {
 dependencies {
     implementation(projects.core.designSystem)
     implementation(projects.core.common)
-
+    implementation(projects.domain.sign)
     implementation(libs.googleid)
+    implementation(libs.gms.play.services.auth)
 }
