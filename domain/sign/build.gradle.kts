ec44387ce35d11b18eb7abad7dff39df1ea2834c
easyhz <jiton3@naver.com>
2024-08-10 10:48:31 +0900

    chore: domain auth 모듈 추가 #52

---
+++ domain/sign/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.noffice.android.library)
+    alias(libs.plugins.noffice.android.hilt)
+    alias(libs.plugins.noffice.android.application.test)
+}
+
+android {
+    namespace = "com.easyhz.noffice.domain.sign"
+}
+
+dependencies {
+    api(projects.core.model)
+    implementation(projects.core.common)
+    implementation(projects.data.auth)
+    implementation(libs.googleid)
+    implementation(libs.gms.play.services.auth)
+}
