d99af918f3bdee83eb21175de74be8297116fda1
easyhz <jiton3@naver.com>
2024-08-12 01:41:16 +0900

    refactor: 띄어쓰기 제거 #52

--- domain/sign/src/main/java/com/easyhz/noffice/domain/sign/usecase/LoginUseCase.kt
+++ domain/sign/src/main/java/com/easyhz/noffice/domain/sign/usecase/LoginUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val loginRepository: LoginRepository
-): BaseUseCase<AuthParam , Unit>() {
+): BaseUseCase<AuthParam, Unit>() {
     override suspend fun invoke(param: AuthParam): Result<Unit> =
         loginRepository.login(param.context, param.providerName)
 }
