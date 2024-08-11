3043719a1e6922dbe7ab7c6c1b549db5300d7a2a
easyhz <jiton3@naver.com>
2024-08-11 22:53:12 +0900

    chore: datastore 모듈 의존성 추가 #52

--- data/auth/build.gradle.kts
+++ data/auth/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 
 dependencies {
     implementation(projects.core.common)
+    implementation(projects.core.datastore)
     implementation(projects.core.network)
     implementation(projects.core.model)
 
