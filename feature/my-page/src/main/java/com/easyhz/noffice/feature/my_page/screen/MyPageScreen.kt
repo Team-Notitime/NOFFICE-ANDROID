52e560da17b1e3ff65cda9e71f4a3648d9451dc3
easyhz <jiton3@naver.com>
2024-08-18 18:02:32 +0900

    fix: bottomSheet padding 수정 #89

--- feature/my-page/src/main/java/com/easyhz/noffice/feature/my_page/screen/MyPageScreen.kt
+++ feature/my-page/src/main/java/com/easyhz/noffice/feature/my_page/screen/MyPageScreen.kt
@@ -13,9 +13,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
