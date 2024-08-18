995264af650a7f8e0cffb98e3f31bfad243e939b
easyhz <jiton3@naver.com>
2024-08-18 17:00:48 +0900

    feat: announcement api 명시 #50

--- core/network/src/main/java/com/easyhz/noffice/core/network/api/announcement/AnnouncementService.kt
+++ core/network/src/main/java/com/easyhz/noffice/core/network/api/announcement/AnnouncementService.kt
@@ -2,8 +2,10 @@ package com.easyhz.noffice.core.network.api.announcement
 
 import com.easyhz.noffice.core.network.model.request.announcement.AnnouncementRequest
 import com.easyhz.noffice.core.network.model.response.announcement.AnnouncementResponse
+import com.easyhz.noffice.core.network.model.response.announcement.AnnouncementTaskResponse
 import com.easyhz.noffice.core.network.util.NofficeResult
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -15,8 +17,28 @@ interface AnnouncementService {
         @Body request: AnnouncementRequest
     ): NofficeResult<AnnouncementResponse>
 
+    /* 조직에 발급된 노티 열람*/
     @GET("/api/v1/announcement/{announcementId}")
     suspend fun fetchAnnouncement(
         @Path("announcementId") announcementId: Int
     ): NofficeResult<AnnouncementResponse>
+
+    /* 노티 수정 */
+    @POST("/api/v1/announcement/{announcementId}")
+    suspend fun updateAnnouncement(
+        @Path("announcementId") announcementId: Int,
+        @Body request: AnnouncementRequest
+    ): NofficeResult<AnnouncementResponse>
+
+    /* 노티 삭제 */
+    @DELETE("/api/v1/announcement/{announcementId}")
+    suspend fun deleteAnnouncement(
+        @Path("announcementId") announcementId: Int,
+    ): NofficeResult<Unit>
+
+    /* 노티에 발급된 투두 조회 */
+    @GET("/api/v1/announcement/{announcementId}/tasks")
+    suspend fun fetchAnnouncementTask(
+        @Path("announcementId") announcementId: Int,
+    ): NofficeResult<AnnouncementTaskResponse>
 }
---
+++ core/network/src/main/java/com/easyhz/noffice/core/network/model/response/task/TaskResponse.kt
@@ -0,0 +1,7 @@
+package com.easyhz.noffice.core.network.model.response.task
+
+data class TaskResponse(
+    val announcementId: Int,
+    val content: String,
+    val id: Int
+)
