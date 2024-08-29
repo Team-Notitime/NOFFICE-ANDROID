ea05fb47d216c89ac5a9193c4949bafad94e9341
easyhz <jiton3@naver.com>
2024-08-29 13:49:40 +0900

    feat: 회원 탈퇴 api 연결 (#176)

--- data/auth/src/main/java/com/easyhz/noffice/data/auth/repository/auth/AuthRepository.kt
+++ data/auth/src/main/java/com/easyhz/noffice/data/auth/repository/auth/AuthRepository.kt
@@ -5,4 +5,5 @@ import android.content.Context
 interface AuthRepository {
     suspend fun login(context: Context, provider: String): Result<Unit>
     suspend fun logout(context: Context): Result<Unit>
+    suspend fun withdraw(context: Context): Result<Unit>
 }
