fee47c6428688429ae9def530f959e11dc4088de
easyhz <jiton3@naver.com>
2024-08-31 11:01:16 +0900

    fix: 로깅 추가 (#187)

--- domain/home/src/main/java/com/easyhz/noffice/domain/home/usecase/splash/GetSplashInfoUseCase.kt
+++ domain/home/src/main/java/com/easyhz/noffice/domain/home/usecase/splash/GetSplashInfoUseCase.kt
@@ -1,6 +1,7 @@
 package com.easyhz.noffice.domain.home.usecase.splash
 
 import com.easyhz.noffice.core.common.base.BaseUseCase
+import com.easyhz.noffice.core.common.util.errorLogging
 import com.easyhz.noffice.core.model.splash.EnterScreenType
 import com.easyhz.noffice.data.member.repository.user.UserRepository
 import javax.inject.Inject
@@ -10,16 +11,19 @@ class GetSplashInfoUseCase @Inject constructor(
 ): BaseUseCase<Unit, EnterScreenType>() {
     override suspend fun invoke(param: Unit): Result<EnterScreenType> = runCatching {
         userRepository.getIsFirstRun().getOrElse {
+            errorLogging(this.javaClass.name, "Error getting isFirstRun", it)
             return@runCatching EnterScreenType.ONBOARDING
         }.takeIf { it }?.let {
             return@runCatching EnterScreenType.ONBOARDING
         }
 
         userRepository.getMemberId().getOrElse {
+            errorLogging(this.javaClass.name, "Error getting memberId", it)
             return@runCatching EnterScreenType.LOGIN
         }
 
         userRepository.getMemberName().getOrElse {
+            errorLogging(this.javaClass.name, "Error getting memberName", it)
             return@runCatching EnterScreenType.SIGN_UP
         }
 
