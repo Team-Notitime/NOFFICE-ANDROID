d6d75d2246ceb828c44894bd02ca44cd3f4cc538
easyhz <jiton3@naver.com>
2024-09-01 20:55:34 +0900

    fix: 버전 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.easyhz.noffice"
-        versionCode = 23
+        versionCode = 25
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
